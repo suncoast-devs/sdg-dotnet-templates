6e1fc5c8495ca6f6e4213164aa1a650f63a6e95b
Gavin Stark <gavin@gstark.com>
2020-07-09 08:18:47 -0400

    Moving UseHttpsRedirection to production only
    This allows us to use http in development and avoid issues with the self-signed cert of dotnet

--- sdg-dotnet-react-template/SampleSite/Startup.cs
+++ sdg-dotnet-react-template/SampleSite/Startup.cs
@@ -64,11 +64,14 @@ namespace SampleSite
             else
             {
                 app.UseExceptionHandler("/Error");
+
+                // Only enforce https in production
+                app.UseHttpsRedirection();
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
             app.UseSwagger();
