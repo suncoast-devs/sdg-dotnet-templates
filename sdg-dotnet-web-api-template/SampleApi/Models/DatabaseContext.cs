263cd73c29aaa64580bfd594b5942a8197db79f5
Gavin Stark <gavin@gstark.com>
2020-07-13 22:45:09 -0400

    Updates the database context

--- sdg-dotnet-web-api-template/SampleApi/Models/DatabaseContext.cs
+++ sdg-dotnet-web-api-template/SampleApi/Models/DatabaseContext.cs
@@ -13,7 +13,7 @@ namespace SampleApi.Models
         // Change this to true if you want to have logging of SQL statements in development
         private static bool LOG_SQL_STATEMENTS_IN_DEVELOPMENT = false;
 
-
+        // Add database tables here
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
