d8a3be5a0db6eac33211c0fa70376f2fafbe1276
Gavin Stark <gavin@gstark.com>
2020-07-13 22:58:02 -0400

    Removes unused using statments

--- sdg-dotnet-web-api-template/SampleApi/Startup.cs
+++ sdg-dotnet-web-api-template/SampleApi/Startup.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using SampleApi.Models;
 
@@ -42,7 +36,9 @@ namespace SampleApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            } else {
+            }
+            else
+            {
                 // Only enforce https in production
                 app.UseHttpsRedirection();
 
